2fa1bae1fcae716dd50449df9c9b97a93f14b0f1
vanderlan carvalho <vanderlan_go2010@hotmail.com>
2021-10-13 21:25:57 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppTemplateSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppTemplateSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
